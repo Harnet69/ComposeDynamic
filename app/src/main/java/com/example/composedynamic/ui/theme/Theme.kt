10b00524bce92cabfe818af3f78368195986a3f2
kokhanau <vital.kokhanau@neptis.pl>
2021-08-08 11:56:58 +0200

    Dependencies update

--- app/src/main/java/com/example/composedynamic/ui/theme/Theme.kt
+++ app/src/main/java/com/example/composedynamic/ui/theme/Theme.kt
@@ -28,10 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun ComposeDynamicTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit
-) {
+fun ComposeDynamicTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
